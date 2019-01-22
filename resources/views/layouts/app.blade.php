540068a4428317d2c2fdd63cbfe3b569f1c24f88
iamkarim <sarumi329@gmail.com>
2019-01-22 20:12:09 +0100

    changed the title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <title>{{config('app.name', 'NhamesHospital')}}</title>
+        <title>{{config('app.name', 'Nhames Hospital')}}</title>
 
     </head>
     <body>
